17294eefce1469034a85a0be93ac80e3febba9a2
Vivek Tarun <vtarun@users.noreply.github.com>
2024-05-27 12:06:39 +0530

    Update Root.js

--- Root.js
+++ Root.js
@@ -1,8 +1,29 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
 
 import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
+import { getTokenDuration } from "../util/auth";
 
 function Rootlayout(){
+    const submit = useSubmit();
+
+    const token = useLoaderData();
+    useEffect(()=>{
+        if(!token){
+            return;
+        }
+        if(token === 'EXPIRED'){
+            submit(null, {action:"/logout", method:'post'})
+            return null;
+        }
+
+        const tokenDuration = getTokenDuration();
+
+        setTimeout(()=>{
+            submit(null, {action:"/logout", method:'post'})
+        }, tokenDuration)
+    }, [token, submit]);
+
     return <>
         <MainNavigation />
         <main>
